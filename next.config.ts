1107885d5dfede65ea4ca6eedee550870440d14e
Mottuqe Brid <mottuqebrid@gmail.com>
2026-02-03 15:33:57 +0600

    Add DaisyUI integration and update project dependencies
    - Introduced DaisyUI as a dependency in package.json and package-lock.json.
    - Configured Next.js to allow remote images from any domain for enhanced image handling.
    - Added VSCode settings for Prettier formatting on save.
    - Updated globals.css to include DaisyUI plugin.
    - Modified Home component in page.tsx to utilize a card layout with an image and actions.
    - Added logo.ico and logo.jpg assets to the public directory.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // domains: ["img.daisyui.com"],
+    remotePatterns: [
+      {
+        protocol: "https", // Enforces HTTPS for security
+        hostname: "**", // Allows any domain
+        port: "",
+        pathname: "**", // Allows any path
+      },
+    ],
+  },
 };
 
 export default nextConfig;
